35bc758e9f7e4e6c9c22d6798ebb96094ae24b54
NHTung-0801 <kiritohackiem05@gmail.com>
2025-12-28 23:05:57 +0700

    hoan thien chuc nang nop page

--- backend/submission-service/src/crud.py
+++ backend/submission-service/src/crud.py
@@ -1,87 +1,94 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, selectinload
 from sqlalchemy import desc
 from datetime import datetime
 from . import models, schemas, exceptions
-from .paper_state import get_paper_state
 
 
 # Nghiệp vụ tạo bài báo mới
 def create_paper(
     db: Session, 
     paper_data: schemas.PaperCreate, 
-    submitter_id: int, 
-    file_url: str
+    submitter_id: int
 ) -> models.Paper:
     
-    try:
-        # Tạo bài báo
-        paper = models.Paper(
-            title=paper_data.title,
-            abstract=paper_data.abstract,
-            conference_id=paper_data.conference_id,
-            track_id=paper_data.track_id,
-            submitter_id=submitter_id,
-            is_blind_mode=paper_data.is_blind_mode,
-            status=models.PaperStatus.SUBMITTED,
-            submitted_at=datetime.utcnow()
-            
-        )
-        db.add(paper)
-        db.flush()
+    # Tạo bài báo
+    db_paper = models.Paper(
+        title=paper_data.title,
+        abstract=paper_data.abstract,
+        conference_id=paper_data.conference_id,
+        track_id=paper_data.track_id,
+        submitter_id=submitter_id,
+        is_blind_mode=paper_data.is_blind_mode,
+        status=models.PaperStatus.SUBMITTED,
+        submitted_at=datetime.utcnow()
+    )
+    db.add(db_paper)
+    db.flush() # Để lấy ID
 
-        # Lưu danh sách đồng giả
-        if paper_data.authors:
-            db_authors = [
-                models.PaperAuthor(
-                    paper_id=db_paper.id,
-                    full_name=author.full_name,
-                    email=author.email,
-                    organization=author.organization,
-                    is_corresponding=author.is_corresponding,
-                    user_id=author.user_id
-                )
-                for author in paper_data.authors
-            ]
-            db.add_all(db_authors)
+    # 2. Lưu danh sách đồng tác giả
+    if paper_data.authors:
+        db_authors = [
+            models.PaperAuthor(
+                paper_id=db_paper.id,
+                full_name=author.full_name,
+                email=author.email,
+                organization=author.organization,
+                is_corresponding=author.is_corresponding,
+                user_id=author.user_id
+            )
+            for author in paper_data.authors
+        ]
+        db.add_all(db_authors)
 
-        # Lưu danh sách chủ đề
-        if paper_data.topics:
-            db_topics = [
-                models.PaperTopic(
-                    paper_id=db_paper.id,
-                    topic_id=t.topic_id
-                )
-                for t in paper_data.topics
-            ]
-            db.add_all(db_topics)
+    # 3. Lưu danh sách chủ đề
+    if paper_data.topics:
+        db_topics = [
+            models.PaperTopic(
+                paper_id=db_paper.id,
+                topic_id=t.topic_id
+            )
+            for t in paper_data.topics
+        ]
+        db.add_all(db_topics)
 
-        # Tạo phiên bản đầu tiên của bài báo
-        db_version = models.PaperVersion(
-            paper_id=db_paper.id,
-            version_number=1,
-            file_url=file_url,
-            is_camera_ready=False,
-            # Nếu chế độ Blind Mode bật, file này cũng phải được đánh dấu ẩn danh
-            is_anonymous=paper_data.is_blind_mode
-        )
-        db.add(db_version)
-
-        # Lưu toàn bộ thay đổi
-        db.commit()
-        db.refresh(db_paper)
-        return db_paper
-
-    except Exception as e:
-        db.rollback()
-        print(f"Error creating paper: {str(e)}")
-        raise e
+    return db_paper
     
+# Tạo phiên bản đầu tiên
+def create_new_paper_version(
+    db: Session, 
+    paper_id: int, 
+    file_url: str,
+    is_blind_mode: bool,
+    is_camera_ready: bool = False
+)-> models.PaperVersion:
+    
+    # Lấy version mới nhất
+    latest = (
+        db.query(models.PaperVersion)
+        .filter(models.PaperVersion.paper_id == paper_id)
+        .order_by(desc(models.PaperVersion.version_number))
+        .with_for_update()
+        .first()
+    )
+    version_number = 1 if not latest else latest.version_number + 1
+    
+ 
+    db_version = models.PaperVersion(
+        paper_id=paper_id,
+        version_number=version_number,
+        file_url=file_url,
+        is_camera_ready=is_camera_ready,
+        is_anonymous=is_blind_mode
+    )
+    db.add(db_version)
+    return db_version    
 
 # Nghiệp vụ lấy danh sách bài báo của tác giả
 def get_papers_by_author(
     db: Session,
     submitter_id: int
-) -> list[models.Paper]:return (
+) -> list[models.Paper]:
+    return (
         db.query(models.Paper)
         .options(
             selectinload(models.Paper.authors),
